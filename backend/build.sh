f512264b94b5b483934fcc73a767496bb74d2997
virajJ-783 <166743068+virajJ-783@users.noreply.github.com>
2026-03-30 00:16:48 +0530

    Runtime.txt added

--- backend/build.sh
+++ backend/build.sh
@@ -4,10 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-```
-
-Then run once: `git update-index --chmod=+x backend/build.sh`
-
-### 1E. CREATE `backend/runtime.txt` (new file)
-```
-python-3.12.3
