984ddd300cb03598547f99a1d41f1b11d36120f2
virajJ-783 <166743068+virajJ-783@users.noreply.github.com>
2026-03-30 01:13:51 +0530

    Updated build.sh for superuser

--- backend/build.sh
+++ backend/build.sh
@@ -4,3 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser automatically (the "|| true" prevents the build from failing if the user already exists on future deploys)
+python manage.py createsuperuser --noinput || true
